741e37f165c09b5582772b8a52465cf0dc3b2457
bluegecko <kei09doya@gmail.com>
2025-05-27 11:47:54 +0900

    Add Search component with styling and Google icon for improved user experience

--- entrypoints/newtab/components/Search.tsx
+++ entrypoints/newtab/components/Search.tsx
@@ -1,7 +1,15 @@
+import styles from "./Search.module.css";
+
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import Fa from "solid-fa";
+
 export default function Search() {
   return (
-    <div class="montserrat component-box">
-      <input type="text" placeholder="Search..." />
+    <div class={`montserrat component-box ${styles.search}`}>
+      <div class={styles.searchBox}>
+        <Fa icon={faGoogle} />
+        <input class={styles.input} type="text" placeholder="Search..." />
+      </div>
     </div>
   );
 }
